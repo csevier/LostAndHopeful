b56d87e945d0d1829974f34810014f043c0edb67
gmeisinger <gmeisinger@gmail.com>
2021-01-27 13:42:48 -0500

    basic character controller working
    moves, jumps, collides

---
+++ ggjLostAndHopeful/Assets/Scripts/Character.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Character : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float jumpForce = 8.0f;
+    public float gravity = 20.0f;
+    public Vector3 moveDirection = Vector3.zero;
+    private float groundDistance = 1.1f;
+
+    CharacterController characterController;
+    public StateMachine movementSM;
+    public StandingState standing;
+    public JumpingState jumping;
+    public FallingState falling;
+
+    public Text debug;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        movementSM = new StateMachine();
+        standing = new StandingState(this, movementSM);
+        jumping = new JumpingState(this, movementSM);
+        falling = new FallingState(this, movementSM);
+        movementSM.Initialize(falling);
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundDistance);
+    }
+
+    public void Move(Vector3 moveDirection)
+    {
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        movementSM.CurrentState.HandleInput();
+        movementSM.CurrentState.LogicUpdate();
+        debug.text = movementSM.CurrentState.GetType().Name;
+    }
+
+    void FixedUpdate()
+    {
+        movementSM.CurrentState.PhysicsUpdate();
+    }
+}
---
+++ ggjLostAndHopeful/Assets/Scripts/States/StandingState.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingState : GroundedState
+{
+
+    private bool jump;
+
+    public StandingState(Character character, StateMachine stateMachine) : base(character, stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        speed = character.speed;
+        jump = false;
+    }
+
+    public override void HandleInput()
+    {
+        base.HandleInput();
+        jump = Input.GetButtonDown("Jump");
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (jump)
+        {
+            stateMachine.ChangeState(character.jumping);
+        }
+        else if (!character.IsGrounded())
+        {
+            stateMachine.ChangeState(character.falling);
+        }
+    }
+
+}
