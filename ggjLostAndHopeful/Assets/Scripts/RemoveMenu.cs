06813f854f6de7517dcaefa42178802a4631eeb2
Cody Sevier <sevierit@gmail.com>
2021-01-28 15:10:46 -0600

    nice wave sound, binded socket to 0.0.0.0:7777

---
+++ ggjLostAndHopeful/Assets/Scripts/RemoveMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class RemoveMenu : NetworkBehaviour
+{
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        GameObject camera = GameObject.Find("Main Camera");
+        if (camera != null)
+        {
+            Destroy(camera);
+        }
+        
+        GameObject man = GameObject.Find("LonelyMan");
+        if (man != null)
+        {
+            Destroy(man);
+        }
+    }
+}
