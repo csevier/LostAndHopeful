86b64b6fae2e84de6d08639bb714012842510869
jayaarrgh <jrrdmb@gmail.com>
2021-01-30 22:44:52 -0600

    OrbPlacer first go, needs networked

---
+++ ggjLostAndHopeful/Assets/Scripts/OrbPlacer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbPlacer : MonoBehaviour
+{
+    public Terrain terrain;
+    public GameObject orb;
+    public float radius;
+    public int amount;
+
+    void Start()
+    {
+        CreateAreaOfOrbs();
+    }
+
+    void CreateAreaOfOrbs()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 pos = transform.position;
+            pos = new Vector3(pos.x + Random.Range(-radius, radius), 0.0f, pos.z + Random.Range(-radius, radius));
+            pos.y = terrain.SampleHeight(pos) + terrain.transform.position.y;
+            GameObject oneOrb = Instantiate(orb, pos, Quaternion.identity);
+            // TODO? take collider y into account
+            //oneOrb.transform.position += new Vector3(0.0f, oneOrb.transform.GetComponent<Collider>().bounds.size.y / 2, 0.0f);
+        }
+    }
+}
